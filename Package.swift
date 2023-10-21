dec9471aebd36f63f6c57f56fa4c713dd91d8f4c
Pol Chen <pol.chen3@gmail.com>
2023-10-21 14:34:41 +0900

    Add back test target to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,15 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WalletKit",
     platforms: [
-        .iOS(.v11),
-        .macOS(.v10_13),
-        .tvOS(.v11),
-        .watchOS(.v4),
+        .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WalletKit",
             targets: ["WalletKit"]
@@ -23,11 +21,15 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WalletKit",
-            dependencies: ["AnyCodable", ],
+            dependencies: ["AnyCodable"],
             path: "Sources/WalletKit"
+        ),
+        .testTarget(
+            name: "WalletKitTests",
+            dependencies: ["WalletKit"]
         ),
     ]
 )
