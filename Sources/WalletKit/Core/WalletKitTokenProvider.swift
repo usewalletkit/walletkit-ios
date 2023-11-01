0cb1b53b7db738732276d329429f2b84efc14c30
Pol Chen <pol.chen3@gmail.com>
2023-11-01 23:44:40 +0900

    Implement token provider to provide access token from WalletKit source

---
+++ Sources/WalletKit/Core/WalletKitTokenProvider.swift
@@ -0,0 +1,38 @@
+//
+//  WalletKitTokenProvider.swift
+//  
+//
+//  Created by Pol Chen on 2023/10/31.
+//
+
+import Foundation
+
+final class WalletKitTokenProvider: TokenProviding {
+
+    func getAccessToken(completion: @escaping ((String?) -> Void)) {
+        let sessionManager = SessionManager()
+        guard let session = sessionManager.retrieveSession() else {
+            completion(nil)
+            return
+        }
+
+        guard sessionManager.shouldRefreshSession(session) else {
+            completion(session.accessToken)
+            return
+        }
+
+        let refreshTokenRequest = UsersRefreshTokenRequest(
+            sessionId: session.id,
+            refreshToken: session.refreshToken
+        )
+        UsersAPI.usersRefreshToken(usersRefreshTokenRequest: refreshTokenRequest) { result in
+            switch result {
+            case .success(let session):
+                sessionManager.storeSession(session)
+                completion(session.accessToken)
+            case .failure:
+                completion(nil)
+            }
+        }
+    }
+}
