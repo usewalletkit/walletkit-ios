0341037dfe1efd9d0b909e46929febe3a4f6a299
Pol Chen <pol.chen3@gmail.com>
2023-11-01 23:43:20 +0900

    Add decorated UsersAPI to store and retrieve session

---
+++ Sources/WalletKit/Core/DecoratedUsersAPI.swift
@@ -0,0 +1,55 @@
+//
+//  DecoratedUsersAPI.swift
+//  
+//
+//  Created by Pol Chen on 2023/10/30.
+//
+
+import Foundation
+#if canImport(AnyCodable)
+import AnyCodable
+#endif
+
+public class DecoratedUsersAPI: UsersAPI {
+
+    /// Returns the current session, if present.
+    ///
+    /// Only available when `tokenSource` is configured as the default `.walletkit`.
+    /// For other `tokenSource`, developers should get the session from the corresponding auth provider.
+    public static var currentSession: Session? {
+        guard WalletKitAPI.tokenSource == .walletkit else {
+            return nil
+        }
+
+        let sessionManager = SessionManager()
+        return sessionManager.retrieveSession()
+    }
+
+    @discardableResult
+    public override class func usersLoginAnonymously(apiResponseQueue: DispatchQueue = WalletKitAPI.apiResponseQueue, completion: @escaping ((Result<Session, ErrorResponse>) -> Void)) -> RequestTask {
+        let modifiedCompletion: ((Result<Session, ErrorResponse>) -> Void) = { result in
+            switch result {
+            case .success(let session):
+                SessionManager().storeSession(session)
+            case .failure:
+                break
+            }
+            completion(result)
+        }
+        return super.usersLoginAnonymously(apiResponseQueue: apiResponseQueue, completion: modifiedCompletion)
+    }
+
+    @discardableResult
+    public override class func usersVerifyLogin(usersVerifyLoginRequest: UsersVerifyLoginRequest, apiResponseQueue: DispatchQueue = WalletKitAPI.apiResponseQueue, completion: @escaping ((Result<Session, ErrorResponse>) -> Void)) -> RequestTask {
+        let modifiedCompletion: ((Result<Session, ErrorResponse>) -> Void) = { result in
+            switch result {
+            case .success(let session):
+                SessionManager().storeSession(session)
+            case .failure:
+                break
+            }
+            completion(result)
+        }
+        return super.usersVerifyLogin(usersVerifyLoginRequest: usersVerifyLoginRequest, apiResponseQueue: apiResponseQueue, completion: modifiedCompletion)
+    }
+}
