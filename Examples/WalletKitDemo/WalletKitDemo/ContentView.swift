3a31cefa454997ba24570b3f8bd658bc3f64b039
Pol Chen <pol.chen3@gmail.com>
2023-11-03 15:07:26 +0900

    Implement list wallets for demo

--- Examples/WalletKitDemo/WalletKitDemo/ContentView.swift
+++ Examples/WalletKitDemo/WalletKitDemo/ContentView.swift
@@ -10,12 +10,30 @@ import WalletKit
 
 struct ContentView: View {
 
-    @State private var userSession: Session?
+    @State private var userSession: Session? = WalletKit.users.currentSession
+    @State private var walletList: [ListWalletsResponseItem] = []
+
     @State private var displayingError: Error?
 
     var body: some View {
         NavigationView {
             Form {
+                Section {
+                    if userSession != nil, walletList.isEmpty {
+                        Button {
+                            print("Create wallet")
+                        } label: {
+                            HStack {
+                                Image(systemName: "plus.square")
+                                Text("Create a Wallet")
+                            }
+                        }
+                    } else {
+                        ForEach(walletList, id: \.id) { wallet in
+                            Text("Wallet: \(wallet.address)")
+                        }
+                    }
+                }
                 Section {
                     if let userSession {
                         Text("User ID: \(userSession.userId)")
@@ -49,6 +67,9 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onAppear {
+                    listWallets()
+                }
             }
             .navigationTitle("WalletKit")
         }
@@ -59,6 +80,17 @@ struct ContentView: View {
             switch result {
             case .success(let session):
                 userSession = session
+            case .failure(let error):
+                displayingError = error
+            }
+        }
+    }
+
+    private func listWallets() {
+        WalletKit.wallets.walletsList { result in
+            switch result {
+            case .success(let list):
+                walletList = list
             case .failure(let error):
                 displayingError = error
             }
