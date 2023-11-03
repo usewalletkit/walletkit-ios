a46476eb59f9ba4d9c618a55100aab23639ea2c8
Pol Chen <pol.chen3@gmail.com>
2023-11-03 10:23:39 +0900

    Create WalletKitDemo project

---
+++ Examples/WalletKitDemo/WalletKitDemo/WalletKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletKitDemoApp.swift
+//  WalletKitDemo
+//
+//  Created by Pol Chen on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct WalletKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
