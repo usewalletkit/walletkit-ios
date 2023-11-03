74c233a46c7adbc6a54b123e002467cb49b7bcdc
Pol Chen <pol.chen3@gmail.com>
2023-11-03 14:41:17 +0900

    Implement sign in anonymously for demo

--- Examples/WalletKitDemo/WalletKitDemo/ContentView.swift
+++ Examples/WalletKitDemo/WalletKitDemo/ContentView.swift
@@ -6,16 +6,63 @@
 //
 
 import SwiftUI
+import WalletKit
 
 struct ContentView: View {
+
+    @State private var userSession: Session?
+    @State private var displayingError: Error?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    if let userSession {
+                        Text("User ID: \(userSession.userId)")
+                        Button(role: .destructive) {
+                            print("Sign out")
+                        } label: {
+                            HStack {
+                                Image(systemName: "rectangle.portrait.and.arrow.forward")
+                                    .frame(width: 20)
+                                Text("Log Out")
+                            }
+                        }
+                    } else {
+                        Button {
+                            signInAnonymously()
+                        } label: {
+                            HStack {
+                                Image(systemName: "person.and.background.dotted")
+                                    .frame(width: 20)
+                                Text("Sign in anonymously")
+                            }
+                        }
+                        Button {
+                            print("Sign in with email")
+                        } label: {
+                            HStack {
+                                Image(systemName: "person.crop.square.filled.and.at.rectangle")
+                                    .frame(width: 20)
+                                Text("Sign in with email")
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("WalletKit")
         }
-        .padding()
+    }
+
+    private func signInAnonymously() {
+        WalletKit.users.usersLoginAnonymously { result in
+            switch result {
+            case .success(let session):
+                userSession = session
+            case .failure(let error):
+                displayingError = error
+            }
+        }
     }
 }
 
