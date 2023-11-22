25247c567dfe8f374fe6e0245215c635375fa392
Pol Chen <pol.chen3@gmail.com>
2023-11-22 15:50:10 +0900

    Add unit tests for `WalletKitTokenProvider`

---
+++ Tests/WalletKitTests/Core/WalletKitTokenProviderTests.swift
@@ -0,0 +1,73 @@
+//
+//  WalletKitTokenProviderTests.swift
+//  
+//
+//  Created by Pol Chen on 2023/11/22.
+//
+
+import XCTest
+@testable import WalletKit
+
+final class WalletKitTokenProviderTests: XCTestCase {
+
+    private var tokenProvider: WalletKitTokenProvider!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        WalletKitAPI.requestBuilderFactory = MockRequestBuilderFactory()
+        WalletKitAPI.sessionManager = SessionManager(keychainManager: MockKeychainManager())
+        tokenProvider = WalletKitTokenProvider()
+    }
+
+    override func tearDownWithError() throws {
+        WalletKitAPI.requestBuilderFactory = URLSessionRequestBuilderFactory()
+        WalletKitAPI.sessionManager = SessionManager()
+        try super.tearDownWithError()
+    }
+
+    func testGetAccessTokenNil() {
+        let sessionManager = WalletKitAPI.sessionManager
+        sessionManager.removeSession()
+        XCTAssertNil(sessionManager.retrieveSession())
+
+        tokenProvider.getAccessToken { token in
+            XCTAssertNil(token)
+        }
+    }
+
+    func testGetAccessTokenValid() {
+        let sessionManager = WalletKitAPI.sessionManager
+        let base: TimeInterval = Session.expirationThreshold
+
+        let session = Session.fixture(
+            accessToken: "VALID_ACCESS_TOKEN",
+            accessTokenExpiresAt: Date(timeIntervalSinceNow: base + 5),
+            refreshTokenExpiresAt: Date(timeIntervalSinceNow: base + 5)
+        )
+        XCTAssertFalse(sessionManager.shouldRefreshSession(session))
+        sessionManager.storeSession(session)
+
+        tokenProvider.getAccessToken { token in
+            XCTAssertEqual(token, session.accessToken)
+            XCTAssertEqual(token, "VALID_ACCESS_TOKEN")
+        }
+    }
+
+    func testGetAccessTokenRefreshed() {
+        let sessionManager = WalletKitAPI.sessionManager
+        let base: TimeInterval = Session.expirationThreshold
+
+        let session = Session.fixture(
+            accessToken: "EXPIRED_ACCESS_TOKEN",
+            accessTokenExpiresAt: Date(timeIntervalSinceNow: base - 5),
+            refreshTokenExpiresAt: Date(timeIntervalSinceNow: base + 5)
+        )
+        XCTAssertTrue(sessionManager.shouldRefreshSession(session))
+        sessionManager.storeSession(session)
+
+        tokenProvider.getAccessToken { token in
+            XCTAssertNotEqual(token, session.accessToken)
+            XCTAssertEqual(token, "TEST_ACCESS_TOKEN")
+        }
+    }
+}
